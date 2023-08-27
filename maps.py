7caabb75f5dbb64e9f9de8228f5ac132706cf380
ds <pushin.st@gmail.com>
2023-08-27 06:18:50 +0300

    feat: refactoring code, added methods

--- maps.py
+++ maps.py
@@ -1,118 +1,88 @@
 import urllib.request
 import datetime
-import ssl
+import os
+
+absPath = f'{os.getcwd()}/img/'
+if not os.path.exists(absPath):
+    os.mkdir(absPath)
 
 class HourDat():
     def __init__(self, hour, isToday):
         self.hour = hour
         self.isToday = isToday
 
-def downloadOne(arrayHours):
+def isToday(hourNow, hourLimit):
+    if hourNow > hourLimit:
+        return True
+    else:
+        return False
+
+def downloadOneStep(arrayHours):
     for hourObj in arrayHours:
         url = f'https://meteoinfo.ru/hmc-input/mapsynop/Analiz{hourObj.hour}h.png'
         # Today or Prevday
         dateForFileName = datetime.date.today() if hourObj.isToday else datetime.date.today() - datetime.timedelta(days=1)
-        fname = f'{dateForFileName.strftime("%d%m")}{hourObj.hour}_PR.png'
+        fname = f'{absPath}{dateForFileName.strftime("%d%m")}{hourObj.hour}_PR.png'
         urllib.request.urlretrieve(url, fname)
 
-#downloadOne([HourDat('00',True)])
-
-
-now = datetime.datetime.now()
-hour_now = now.hour
-print(hour_now)
-print("Hello world!")
-
-if hour_now > 15:
-    prizem_t = ['00', '06']
-    prizem_y = ['12', '18']
-
-    for i in prizem_t:
-        url = f'https://meteoinfo.ru/hmc-input/mapsynop/Analiz{i}h.png'
-        fname = f'{datetime.date.today().strftime("%d%m")}{i}_PR.png'
+def downloadTwoStep(arrayHours):
+    for hourObj in arrayHours:
+        url = f'http://www.meteocenter.net/circ/rotate/UNTT{hourObj.hour}.png'
+        # Today or Prevday
+        dateForFileName = datetime.date.today() if hourObj.isToday else datetime.date.today() - datetime.timedelta(days=1)
+        fname = f'{absPath}{dateForFileName.strftime("%d%m")}{hourObj.hour}_K.png'
         urllib.request.urlretrieve(url, fname)
 
-    for i in prizem_y:
-        url = f'https://meteoinfo.ru/hmc-input/mapsynop/Analiz{i}h.png'
-        fname = f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_PR.png'
-        urllib.request.urlretrieve(url, fname)
-else:
-    prizem_t = ['00']
-    prizem_y = ['06', '12', '18']
+def downloadThreeStep(arrayHours, arrayHeights):
+    for hourObj in arrayHours:
+        # Today or Prevday
+        dateForFileName = datetime.date.today() if hourObj.isToday else datetime.date.today() - datetime.timedelta(days=1)
+        for height in arrayHeights:
+            url = f'https://meteoinfo.ru/hmc-input/mapsynop/AT-{height}-{hourObj.hour}.png'
 
-    for i in prizem_t:
-        url = f'https://meteoinfo.ru/hmc-input/mapsynop/Analiz{i}h.png'
-        fname = f'{datetime.date.today().strftime("%d%m")}{i}_PR.png'
-        urllib.request.urlretrieve(url, fname)
-
-    for i in prizem_y:
-        url = f'https://meteoinfo.ru/hmc-input/mapsynop/Analiz{i}h.png'
-        fname = f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_PR.png'
-        urllib.request.urlretrieve(url, fname)
-
-
-if hour_now > 17:
-    rotate_t = ['00', '03', '06', '09']
-    rotate_y = ['12', '15', '18', '21']
-    for i in rotate_t:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url, f'{datetime.date.today().strftime("%d%m")}{i}_K.png')
-
-    for i in rotate_y:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url,
-                                   f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_K.png')
-elif hour_now > 15:
-    rotate_t = ['00', '03', '06']
-    rotate_y = ['09', '12', '15', '18', '21']
-    for i in rotate_t:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url, f'{datetime.date.today().strftime("%d%m")}{i}_K.png')
-
-    for i in rotate_y:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url,
-                                   f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_K.png')
-elif hour_now > 13:
-    rotate_t = ['00', '03']
-    rotate_y = ['06', '09', '12', '15', '18', '21']
-    for i in rotate_t:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url, f'{datetime.date.today().strftime("%d%m")}{i}_K.png')
-
-    for i in rotate_y:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url,
-                                   f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_K.png')
-else:
-    rotate_t = ['00']
-    rotate_y = ['03', '06', '09', '12', '15', '18', '21']
-    for i in rotate_t:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url, f'{datetime.date.today().strftime("%d%m")}{i}_K.png')
-
-    for i in rotate_y:
-        url = f'http://www.meteocenter.net/circ/rotate/UNTT{i}.png'
-        urllib.request.urlretrieve(url,
-                                   f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_K.png')
-
-hours = ['00', '12']
-height = [925, 850, 700, 500, 400, 300, 200, 100]
-
-for i in hours:
-    if i == '00':
-        for k in height:
-            url = f'https://meteoinfo.ru/hmc-input/mapsynop/AT-{k}-{i}.png'
-            if k % 10 == 0:
-                fname = f'{datetime.date.today().strftime("%d%m")}{i}_{int(k / 10)}.png'
+            if height % 10 == 0:
+                fname = f'{absPath}{dateForFileName.strftime("%d%m")}{hourObj.hour}_{int(height / 10)}.png'
             else:
-                fname = f'{datetime.date.today().strftime("%d%m")}{i}_{k}.png'
-            urllib.request.urlretrieve(url, fname)
-    else:
-        for k in height:
-            url = f'https://meteoinfo.ru/hmc-input/mapsynop/AT-{k}-{i}.png'
-            if k % 10 == 0:
-                fname = f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_{int(k / 10)}.png'
-            else:
-                fname = f'{(datetime.date.today() - datetime.timedelta(days=1)).strftime("%d%m")}{i}_{k}.png'
-            urllib.request.urlretrieve(url, fname)
+                fname = f'{absPath}{dateForFileName.strftime("%d%m")}{hourObj.hour}_{height}.png'
+            urllib.request.urlretrieve(url, fname)                
+
+def oneStep(hourNow):
+    arrayHours = [
+        HourDat('00',True),
+        HourDat('06',isToday(hourNow, 15)),
+        HourDat('12',False),
+        HourDat('18',False),
+        ]
+    downloadOneStep(arrayHours)
+
+def twoStep(hourNow):
+    arrayHours = [
+            HourDat('00',True),
+            HourDat('03',isToday(hourNow, 13)),
+            HourDat('06',isToday(hourNow, 15)),
+            HourDat('09',isToday(hourNow, 17)),
+            HourDat('12',False),
+            HourDat('15',False),
+            HourDat('18',False),
+            HourDat('21',False),
+        ]
+    downloadTwoStep(arrayHours)
+
+def threeStep():
+    height = [925, 850, 700, 500, 400, 300, 200, 100]
+    arrayHours = [
+            HourDat('00',True),
+            HourDat('12',False),
+        ]
+    downloadThreeStep(arrayHours, height)
+
+def main():
+    hour_now = datetime.datetime.now().hour
+    print(hour_now)
+
+    oneStep(hour_now)
+    twoStep(hour_now)
+    threeStep(hour_now)
+
+
+main()
